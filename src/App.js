766ecd4d9a673a2cab5be220ac73fc9162027a67
ASETKU\PID437 <festine.putri@asetku.com>
2026-01-27 18:06:51 +0700

    template

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./index.css";
+import "./output.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
