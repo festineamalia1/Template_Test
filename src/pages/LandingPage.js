64d5f1d8da15b615344e1d6ad67e6512ad16c123
ASETKU\PID437 <festine.putri@asetku.com>
2026-01-28 17:41:08 +0700

    push

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+
+export default function LandingPage() {
+  return (
+      <section id="layanan">
+        <div className="container">
+          <div className="row mt-5">
+           <div>Landing Page</div>
+          </div>
+        </div>
+      </section>
+  );
+}
